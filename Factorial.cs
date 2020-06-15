f3fb7740301405841a744cc7ee373b6a89604625
AtomicFrenzy112 <66483196+AtomicFrenzy112@users.noreply.github.com>
2020-06-15 13:28:07 +0100

    Add files via upload

---
+++ Factorial.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Factorial_Finder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*The Factorial of a positive integer, n, is defined as the product of the sequence n, n-1, n-2,
+             * ...1 and the factorial of zero, 0, is defined as being 1.*/
+
+
+            Console.WriteLine("Enter positive integer");
+            int num = Convert.ToInt32(Console.ReadLine());
+            int factorial = num;
+            
+
+            if (num < 0)
+            {
+                Console.WriteLine("Not a positive integer");
+                
+            }
+
+            for (int i = 1; i <num;i++)
+            {           
+                factorial = factorial * (num - i);
+                               
+            }
+
+            if (num == 0)
+            {
+                factorial = 1;
+            }
+            Console.WriteLine(factorial);
+        }
+    }
+}
